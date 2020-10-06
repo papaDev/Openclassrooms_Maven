9f42ac3af33c61459ded35b1ba699843db549c18
Sebastien Fremy <sebastien.fremy1986@gmail.com>
2020-10-06 11:00:28 +0200

    Ajout Code - Chapitre : Décrivez votre projet (ajout code de la video)

--- src/main/java/org/exemple/demo/App.java
+++ src/main/java/org/exemple/demo/App.java
@@ -1,13 +1,30 @@
 package org.exemple.demo;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
+    	
         System.out.println( "Hello World!" );
+        
+        Properties vProp = new Properties();
+        InputStream vInputStream = null;
+        try {
+        	vInputStream = App.class.getResourceAsStream("/info.properties");
+        	vProp.load(vInputStream);
+        } finally {
+        	if(vInputStream != null) {
+        		vInputStream.close();
+        	}
+        }
+        
+        System.out.println("Application version : " + vProp.getProperty("org.exemple.demo.version", "?"));
     }
 }
